8bddbe18411a40d2a5d35de5b553691871194c30
sohamspawar18 <sohamspawar18@gmail.com>
2025-11-15 14:21:29 +0530

    chore: initial commit (app/layout.tsx)

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
